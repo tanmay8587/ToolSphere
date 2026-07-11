9ec1e66cc38f9df4d37dae88500c4057e7e43430
Tanmay Pratap Singh <tanmaypratapsingh16@gmail.com>
2026-07-11 18:53:36 +0530

    feat(blog): configure Tailwind Typography

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+import typography from "@tailwindcss/typography";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "class",
@@ -21,5 +23,5 @@ export default {
       },
     }
   },
-  plugins: []
+  plugins: [typography]
 };
